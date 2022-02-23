207cdd9387e8911d052dbf605e4e5a830871b4c3
Stephen Ouldhouse <stephenouldhouse@Stephens-MacBook-Pro.local>
2022-02-22 17:46:18 -0800

    replaced svg with colored filled in sections, added gradient for background

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,44 @@
 <template>
   <div class="home">
-    <h1>Hello, my name is Stephen</h1>
-    <h2>I'm a programmer and a developer working as as contractor at Goodbeast. I love building beautiful websites with Vue and headless content managment systems. Scroll below to see some examples of my work or get in touch.</h2>
+    <div class="introText">
+      <h1>Hello, my name is Stephen</h1>
+      <h2>I'm a programmer and a developer working as as contractor at Goodbeast. I love building beautiful websites with Vue and headless content managment systems. Scroll below to see some examples of my work or get in touch.</h2>
+    </div>
     <LayeredBackground />
-    <div class="backgroundOpacity"></div>
     <Works/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Works from '@/components/Works.vue'
-import LayeredBackground from '@/components/LayeredBackground.vue'
+import Works from '@/components/Works.vue';
+import LayeredBackground from '@/components/LayeredBackground.vue';
+import { gsap } from "gsap"; 
+import { ScrollTrigger } from "gsap/ScrollTrigger"; 
 export default {
   name: 'Home',
   components: {
       Works,
       LayeredBackground
+  },
+  mounted(){
+		gsap.registerPlugin(ScrollTrigger);
+      gsap.timeline({
+  scrollTrigger: {
+    trigger: ".introText",
+    start: "top top",
+    end: "bottom center",
+    scrub: true,
+    pin: true
   }
+})
+         },
 }
 </script>
 <style scoped>
-.backgroundOpacity{
-  position: absolute;
-background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(2,11,38,1) 50%);
-height: 53vh;
-width: 100%;
-margin-bottom: 57%;
-top: 25;
-top: 132%;
+.home{
+  background: rgb(0,0,0);
+background: linear-gradient(0deg, rgba(0,0,0,1) 12%, rgba(2,11,38,1) 25%, rgba(2,11,38,1) 59%, rgba(152,121,217,1) 87%);
 }
+
 </style>
